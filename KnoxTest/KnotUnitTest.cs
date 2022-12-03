63a6384fa1ec5801f645b5d86c5c3a3bb005132f
johnthebeloved <johnoluwale1@gmail.com>
2022-12-02 19:47:33 -0500

    Created a Test nethod to validate static user credentials and updates to project solution solution

--- KnoxTest/KnotUnitTest.cs
+++ KnoxTest/KnotUnitTest.cs
@@ -1,10 +1,18 @@
-﻿namespace KnoxTest;
+﻿using KnoxLogic;
+
+namespace KnoxTest;
 
 [TestClass]
 public class KnotUnitTest
 {
     [TestMethod]
-    public void TestMethod1()
+    public void TestUserCredentials()
     {
+        string userName = "John";
+        string userPwd = "123";
+        string correctUserName = Knot.UserName("John");
+        string correctUserPassword = Knot.UserPassword("123");
+        Assert.AreEqual(userName, correctUserName);
+        Assert.AreEqual(userPwd, correctUserPassword);
     }
 }
