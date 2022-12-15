d00501968e1a2465c27bf9695567e68471d1d84a
johnthebeloved <johnoluwale1@gmail.com>
2022-12-15 18:59:05 -0500

    Created API Response class to get the value from the API

---
+++ KnoxLogic/Responses/APIResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnoxLogic.Responses
+{
+    public class APIResponse
+    {
+        public string APIResponseMessage { get; set; }
+        public bool IsAPIMessageSuccessful { get; set; }
+    }
+    public class APIResponse<T> : APIResponse
+    {
+        public T Value { get; set; }
+    }
+}
