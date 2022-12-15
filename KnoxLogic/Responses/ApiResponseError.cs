0fff6a16c0910d25da73a15d02c9613289c6c327
johnthebeloved <johnoluwale1@gmail.com>
2022-12-15 18:44:38 -0500

    Created Responses folder to store the API Responses. Created an API Response Error Class

---
+++ KnoxLogic/Responses/ApiResponseError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnoxLogic.Responses
+{
+    public class ApiResponseError
+    {
+        public string ResponseMessage { get; set; }
+        public string ResponseError { get; set; }
+        public bool IsResponseSuccessful { get; set; }
+    }
+}
