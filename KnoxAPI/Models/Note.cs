619545939d37d9340f3f4118066c0b897a55890c
tochukwu <A00258497@mycambrian.ca>
2022-12-16 15:33:02 -0500

    :star: API models added

---
+++ KnoxAPI/Models/Note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using KnoxLogic.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace KnoxAPI.Models
+{
+	public class Note
+	{
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        public string? Content { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public string? UserId { get; set; }
+
+        public User? User { get; set; }
+    }
+}
+
---
+++ KnoxUI/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace KnoxUI.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
+
