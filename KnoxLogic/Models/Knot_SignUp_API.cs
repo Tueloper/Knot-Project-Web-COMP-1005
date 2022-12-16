50d1955eb5ed504f805dad99f86b9c3197e38d1f
johnthebeloved <johnoluwale1@gmail.com>
2022-12-16 07:56:34 -0500

    Created a sign up class model

---
+++ KnoxLogic/Models/Knot_SignUp_API.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnoxLogic.Models
+{
+    public class Knot_SignUp_API
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
