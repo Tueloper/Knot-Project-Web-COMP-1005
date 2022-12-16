44a5dbcf254f61fa9cf065cdc01a8fc17c2e4b4f
johnthebeloved <johnoluwale1@gmail.com>
2022-12-16 08:03:18 -0500

    Created an API Result class model

---
+++ KnoxLogic/Models/Knot_Login_API_Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnoxLogic.Models
+{
+    public class Knot_Login_API_Result
+    {
+        public string APIToken { get; set; }
+        public DateTime Token_Expiry_Date { get; set; }
+    }
+}
