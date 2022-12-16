12ceea41896152468af5b892d7f03d9ebe7e312d
johnthebeloved <johnoluwale1@gmail.com>
2022-12-16 18:22:53 -0500

    Created validation for sign-up form

---
+++ KnoxLogic/Validator/Knot_SignUp_API_Validator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using KnoxLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnoxLogic.Validator
+{
+    public class Knot_SignUp_API_Validator:AbstractValidator<Knot_SignUp_API>
+    {
+        public Knot_SignUp_API_Validator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .WithMessage("Name is required")
+                .MaximumLength(20)
+                .WithMessage("Name is required");
+
+            RuleFor(p => p.Email)
+                .NotEmpty()
+                .WithMessage("Email is required")
+                .EmailAddress()
+                .WithMessage("Email address is not valid. Recheck");
+
+            RuleFor(p => p.Password)
+                .NotEmpty()
+                .WithMessage("Password is required")
+                .MaximumLength(10)
+                .WithMessage("Password must be at least 6 characters long");
+        }
+    }
+}
